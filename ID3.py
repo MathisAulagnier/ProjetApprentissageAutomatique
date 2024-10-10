82ca5358f700469c6a4b318c48367317e2e58ff4
mathisaulagnier <mathis.aulagnier@telecomnancy.eu>
2024-10-10 13:08:28 -0400

    import

--- ID3.py
+++ ID3.py
@@ -1,15 +1,22 @@
 import pandas as pd
 import numpy as np
+
+
 from sklearn.preprocessing import (
     KBinsDiscretizer, 
+    FunctionTransformer
 )
 from sklearn.pipeline import (
     Pipeline
 )
 
-from sklearn.compose import ColumnTransformer
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import KBinsDiscretizer, FunctionTransformer
+from sklearn.compose import (
+    ColumnTransformer
+)
+
+from sklearn.impute import (
+    SimpleImputer
+)
 
 
 class ID3:
